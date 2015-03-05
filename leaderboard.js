88e631d0aca22c9f2559389e06e2ca576b566e34
Brett Henderson <brett@bretth.com>
2015-03-05 13:23:07 +1100

    Build skeleton result for monthly distance leaderboards

--- leaderboard.js
+++ leaderboard.js
@@ -69,6 +69,10 @@ function monthFromDate(date) {
 	return date.getFullYear() * 100 + date.getMonth();
 }
 
+function yearFromMonth(month) {
+	return Math.round(month / 100);
+}
+
 function buildMonthsSet(activities) {
 	return buildDateSet(activities, monthFromDate);
 }
@@ -85,7 +89,7 @@ function buildAthletesSet(activities) {
 	return activities.reduce(reduceFunction, []).sort();
 }
 
-function buildSkeleton(yearsSet, athletesSet) {
+function buildSkeleton(yearsSet, monthsSet, athletesSet) {
 	var reduceDistanceById = function(distanceByAthleteId, distanceRecord) {
 		distanceByAthleteId[distanceRecord.athleteId] = distanceRecord;
 		return distanceByAthleteId;
@@ -100,15 +104,40 @@ function buildSkeleton(yearsSet, athletesSet) {
 		// Create an index of athlete distance objects by athlete id.
 		var distanceByAthleteId = distance.reduce(reduceDistanceById, {});
 
-		return { year: currentYear, distance: distance, distanceByAthleteId: distanceByAthleteId };
+		return { year: currentYear, distance: distance, distanceByAthleteId: distanceByAthleteId, month: [] };
 	}).sort(function(a, b) {
-		return a.year < b.year;
+		return b.year - a.year;
 	});
 	// Create an index of year objects by year id.
 	var yearById = yearObj.reduce(function(yearById, yearRecord) {
 		yearById[yearRecord.year] = yearRecord;
 		return yearById;
 	}, {});
+
+	// Build arrays of month objects grouped by year and sorted by reverse chronological time.
+	monthsSet.forEach(function(currentMonth) {
+		// Build an array of athlete distance objects.
+		var distance = athletesSet.map(function(currentAthlete) {
+			return { athleteId: currentAthlete, distance: 0 };
+		});
+		// Create an index of athlete distance objects by athlete id.
+		var distanceByAthleteId = distance.reduce(reduceDistanceById, {});
+
+		// Add the month to the relevant year.
+		var monthObj = { month: currentMonth, distance: distance, distanceByAthleteId: distanceByAthleteId };
+		yearById[yearFromMonth(currentMonth)].month.push(monthObj);
+	});
+	// Sort the month records by reverse chronological time, and create indexes of month objects by month id within each year.
+	yearObj.forEach(function(yearRecord) {
+		yearRecord.month.sort(function(a, b) {
+			return b.month - a.month;
+		});
+		console.log('yearRecord.month sorted: ' + util.stringify(yearRecord.month));
+		yearRecord.monthById = yearRecord.month.reduce(function(monthById, monthRecord) {
+			monthById[monthRecord.month] = monthRecord;
+			return monthById;
+		}, {});
+	});
 
 	return { year: yearObj, yearById: yearById };
 }
@@ -124,7 +153,7 @@ function calculateYearlyDistance(leaderboard, activities) {
 	// Sort distances in descending order.
 	leaderboard.year.forEach(function(year) {
 		year.distance.sort(function(a, b) {
-			return a.distance < b.distance;
+			return b.distance - a.distance;
 		});
 	});
 }
@@ -132,16 +161,18 @@ function calculateYearlyDistance(leaderboard, activities) {
 function buildLeaderboard(activities) {
 	// Build the complete set of years.
 	var yearsSet = buildYearsSet(activities);
+	// Build the complete set of months.
+	var monthsSet = buildMonthsSet(activities);
 	// Build the complete set of athletes.
 	var athletesSet = buildAthletesSet(activities);
 
 	// Build the skeleton leaderboard.
-	var leaderboard = buildSkeleton(yearsSet, athletesSet);
+	var leaderboard = buildSkeleton(yearsSet, monthsSet, athletesSet);
 
 	// Calculate yearly athlete distances.
 	calculateYearlyDistance(leaderboard, activities);
 
-	console.log("leaderboard: " + util.stringify(leaderboard));
+	//console.log("leaderboard: " + util.stringify(leaderboard));
 
 	return leaderboard;
 }
