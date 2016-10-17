aa1f95235636e4988abdacdf092348a8272073b1
Brett Henderson <brett@bretth.com>
2016-10-17 21:06:38 +1100

    Add eslint
    Configure "npm run lint" command.
    Fix all lint issues except console.log.

--- app/db.js
+++ app/db.js
@@ -1,99 +1,99 @@
 'use strict';
 
-var assert = require('assert')
+var assert = require('assert');
 
 // MongoDB database driver.
-var mongoClient = require('mongodb').MongoClient
+var mongoClient = require('mongodb').MongoClient;
 
 // Util module.
 var util = require('./util');
 
 
 module.exports.start = function(url) {
-	// Connect to Mongo DB.
-	var mongodb;
-	mongoClient.connect(url, function(err, db) {
-		assert.equal(null, err);
-		console.log('Connected to MongoDB');
-		mongodb = db;
-	});
+  // Connect to Mongo DB.
+  var mongodb;
+  mongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB');
+    mongodb = db;
+  });
 
-	// Return accessor methods.
-	return {
+  // Return accessor methods.
+  return {
 
-		// Search for items in the specified collection.
-		getItems: function(collection, criteria, callback) {
-			console.log('Searching ' + collection + ' with criteria ' + util.stringify(criteria));
-			mongodb.collection(collection).find(criteria, {}).toArray(function(err, items) {
-				if (err) console.log('Unable to retrieve items\n' + util.stringify(err));
-				//else console.log('Successfully retrieved items\n' + util.stringify(items));
-				callback(err, items);
-			});
-		},
+    // Search for items in the specified collection.
+    getItems: function(collection, criteria, callback) {
+      console.log('Searching ' + collection + ' with criteria ' + util.stringify(criteria));
+      mongodb.collection(collection).find(criteria, {}).toArray(function(err, items) {
+        if (err) console.log('Unable to retrieve items\n' + util.stringify(err));
+        //else console.log('Successfully retrieved items\n' + util.stringify(items));
+        callback(err, items);
+      });
+    },
 
-		// Save or refresh an athlete.
-		saveAthlete: function(athlete, callback) {
-			// Get the documents collection
-			var collection = mongodb.collection('athletes');
-			// Add or update the athlete
-			collection.update(
-				{ id : athlete.id },
-				athlete,
-				{ upsert : true },
-				function(err) {
-					if (err) console.log('Unable to insert athlete\n' + util.stringify(err));
-					//else console.log('Successfully inserted athlete ' + athlete.id);
-					callback(err);
-				}
-			);
-		},
+    // Save or refresh an athlete.
+    saveAthlete: function(athlete, callback) {
+      // Get the documents collection
+      var collection = mongodb.collection('athletes');
+      // Add or update the athlete
+      collection.update(
+        { id : athlete.id },
+        athlete,
+        { upsert : true },
+        function(err) {
+          if (err) console.log('Unable to insert athlete\n' + util.stringify(err));
+          //else console.log('Successfully inserted athlete ' + athlete.id);
+          callback(err);
+        }
+      );
+    },
 
-		// Save or refresh an athlete's token.
-		saveAthleteToken: function(id, token, callback) {
-			var collection = mongodb.collection('tokens');
-			collection.update(
-				{ id: id },
-				{ id: id, token: token },
-				{ upsert: true },
-				function (err) {
-					if (err) console.log('Unable to insert token\n' + util.stringify(err));
-					//else console.log('Successfully inserted token ' + id);
-					callback(err);
-				}
-			);
-		},
+    // Save or refresh an athlete's token.
+    saveAthleteToken: function(id, token, callback) {
+      var collection = mongodb.collection('tokens');
+      collection.update(
+        { id: id },
+        { id: id, token: token },
+        { upsert: true },
+        function (err) {
+          if (err) console.log('Unable to insert token\n' + util.stringify(err));
+          //else console.log('Successfully inserted token ' + id);
+          callback(err);
+        }
+      );
+    },
 
-		deleteActivities: function(athleteId, callback) {
-			mongodb.collection('activities').remove({ 'athlete.id': athleteId }, {}, callback);
-		},
+    deleteActivities: function(athleteId, callback) {
+      mongodb.collection('activities').remove({ 'athlete.id': athleteId }, {}, callback);
+    },
 
-		// Save or refresh a list of activities.
-		saveActivities: function(activities, callback) {
-			var collection = mongodb.collection('activities');
+    // Save or refresh a list of activities.
+    saveActivities: function(activities, callback) {
+      var collection = mongodb.collection('activities');
 
-			// Create recursive function to insert all activities.
-			var insertActivity = function(activities, i, callback) {
-				if (i < activities.length) {
-					var activity = activities[i];
-					collection.update(
-						{ id: activity.id },
-						activity,
-						{ upsert: true },
-						function (err) {
-							if (err) {
-								console.log('Unable to insert activity\n' + util.stringify(err));
-								callback(err);
-							} else {
-								//console.log('Successfully inserted activity ' + activity.id);
-								insertActivity(activities, i + 1, callback);
-							}
-						}
-					);
-				} else callback(null);
-			};
+      // Create recursive function to insert all activities.
+      var insertActivity = function(activities, i, callback) {
+        if (i < activities.length) {
+          var activity = activities[i];
+          collection.update(
+            { id: activity.id },
+            activity,
+            { upsert: true },
+            function (err) {
+              if (err) {
+                console.log('Unable to insert activity\n' + util.stringify(err));
+                callback(err);
+              } else {
+                //console.log('Successfully inserted activity ' + activity.id);
+                insertActivity(activities, i + 1, callback);
+              }
+            }
+          );
+        } else callback(null);
+      };
 
-			// Initiate recursive insert.
-			insertActivity(activities, 0, callback);
-		},
-	}
+      // Initiate recursive insert.
+      insertActivity(activities, 0, callback);
+    },
+  };
 };
--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var fs = require("fs");
+var fs = require('fs');
 
 // Base express framework.
 var express = require('express');
@@ -19,9 +19,9 @@ var exphbs  = require('express-handlebars');
 var config = require('./config');
 // Write strava config out to file in time for loading by the Strava module.
 try {
-	fs.mkdirSync(__dirname + '/data');
+  fs.mkdirSync(__dirname + '/data');
 } catch (err) {
-	if (err.code != 'EEXIST') throw err;
+  if (err.code != 'EEXIST') throw err;
 }
 fs.writeFileSync(__dirname + '/data/strava_config', JSON.stringify(config.strava));
 
@@ -42,111 +42,111 @@ var util = require('./util');
 var db = dbEngine.start(config.mongo.url);
 
 function registerAthlete(stravaCode, callback) {
-	console.log('Registering athlete with code ' + stravaCode);
-	// Exchange the temporary code for an access token.
-	strava.getOAuthToken(stravaCode, function(err, payload) {
-		if (err) {
-			callback(err);
-		} else {
-			// Save athlete information to the database.
-			var athlete = payload.athlete;
-			var token = payload.access_token;
-			db.saveAthlete(athlete, function(err) {
-				if (err) callback(err);
-				else db.saveAthleteToken(athlete.id, token, function(err) {
-					callback(err);
-				});
-			});
-		}
-	});
+  console.log('Registering athlete with code ' + stravaCode);
+  // Exchange the temporary code for an access token.
+  strava.getOAuthToken(stravaCode, function(err, payload) {
+    if (err) {
+      callback(err);
+    } else {
+      // Save athlete information to the database.
+      var athlete = payload.athlete;
+      var token = payload.access_token;
+      db.saveAthlete(athlete, function(err) {
+        if (err) callback(err);
+        else db.saveAthleteToken(athlete.id, token, function(err) {
+          callback(err);
+        });
+      });
+    }
+  });
 }
 
 function registerAthleteToken(stravaToken, callback) {
-	console.log('Registering athlete with token: ' + stravaToken);
-	strava.getAthlete(stravaToken, function(err, athlete) {
-		if (err) callback(err);
-		else {
-			db.saveAthlete(athlete, function(err) {
-				if (err) callback(err);
-				else db.saveAthleteToken(athlete.id, stravaToken, function(err) {
-					callback(err);
-				});
-			});
-		}
-	});
+  console.log('Registering athlete with token: ' + stravaToken);
+  strava.getAthlete(stravaToken, function(err, athlete) {
+    if (err) callback(err);
+    else {
+      db.saveAthlete(athlete, function(err) {
+        if (err) callback(err);
+        else db.saveAthleteToken(athlete.id, stravaToken, function(err) {
+          callback(err);
+        });
+      });
+    }
+  });
 }
 
 // Refresh athlete details in our database.
 function refreshAthlete(athleteId, callback) {
-	console.log('Refreshing athlete ' + athleteId);
-	db.getItems('tokens', { id : athleteId }, function(err, tokens) {
-		if (err) callback(err);
-		else strava.getAthlete(tokens[0].token, function(err, athlete) {
-			if (err) callback(err);
-			else db.saveAthlete(athlete, function(err) {
-				callback(err);
-			});
-		});
-	});
+  console.log('Refreshing athlete ' + athleteId);
+  db.getItems('tokens', { id : athleteId }, function(err, tokens) {
+    if (err) callback(err);
+    else strava.getAthlete(tokens[0].token, function(err, athlete) {
+      if (err) callback(err);
+      else db.saveAthlete(athlete, function(err) {
+        callback(err);
+      });
+    });
+  });
 }
 
 // Refresh an athlete's activities in our database.
 function refreshAthleteActivities(athleteId, callback) {
-	console.log('Refreshing athlete activities ' + athleteId);
-	db.getItems('tokens', { id : athleteId }, function(err, tokens) {
-		if (err) callback(err);
-		else {
-			var pageCallback = function(activities, pageCallbackCallback) {
-				db.saveActivities(activities, function(err) {
-					pageCallbackCallback(err);
-				});
-			}
-			var finalCallback = function(err) {
-				callback(err);
-			}
-			// Delete activities, and then do a full refresh from Strava.
-			db.deleteActivities(athleteId, function(err) {
-				if (err) console.log('Unable to remove activities for athlete ' + athlete.id);
-				else strava.getActivities(tokens[0].token, pageCallback, finalCallback);
-			});
-		}
-	});
+  console.log('Refreshing athlete activities ' + athleteId);
+  db.getItems('tokens', { id : athleteId }, function(err, tokens) {
+    if (err) callback(err);
+    else {
+      var pageCallback = function(activities, pageCallbackCallback) {
+        db.saveActivities(activities, function(err) {
+          pageCallbackCallback(err);
+        });
+      };
+      var finalCallback = function(err) {
+        callback(err);
+      };
+      // Delete activities, and then do a full refresh from Strava.
+      db.deleteActivities(athleteId, function(err) {
+        if (err) console.log('Unable to remove activities for athlete ' + athleteId);
+        else strava.getActivities(tokens[0].token, pageCallback, finalCallback);
+      });
+    }
+  });
 }
 
 function refreshAllAthleteActivities(callback) {
-	var refreshNextAthleteActivities = function(athletes) {
-		if (athletes.length > 0) {
-			var athlete = athletes[0];
-			refreshAthleteActivities(athlete.id, function(err) {
-				if (err) console.log('Unable to refresh athlete ' + athlete.id);
+  var refreshNextAthleteActivities = function(athletes) {
+    if (athletes.length > 0) {
+      var athlete = athletes[0];
+      refreshAthleteActivities(athlete.id, function(err) {
+        if (err) console.log('Unable to refresh athlete ' + athlete.id);
 
-				refreshNextAthleteActivities(athletes.slice(1));
-			});
-		} else {
-			console.log('Completed refreshing all athlete activities');
-			callback();
-		}
-	}
+        refreshNextAthleteActivities(athletes.slice(1));
+      });
+    } else {
+      console.log('Completed refreshing all athlete activities');
+      callback();
+    }
+  };
 
-	console.log('Refreshing all athlete activities');
-	db.getItems('athletes', {}, function(err, athletes) {
-		if (err) callback(err);
-		else refreshNextAthleteActivities(athletes);
-	});
+  console.log('Refreshing all athlete activities');
+  db.getItems('athletes', {}, function(err, athletes) {
+    if (err) callback(err);
+    else refreshNextAthleteActivities(athletes);
+  });
 }
 
 // Send an error message back to the user.
 function sendErrorMessage(res, description) {
-	res.render('error.handlebars', {
-		error : { description : description }
-	});
+  res.render('error.handlebars', {
+    error : { description : description }
+  });
 }
 
 // Send an error message back to the user.
 function sendError(res) {
-	res.render('error.handlebars', {
-		error : { description : "An unexpected error has occurred." }
-	});
+  res.render('error.handlebars', {
+    error : { description : 'An unexpected error has occurred.' }
+  });
 }
 
 // Instantiate express app.
@@ -165,153 +165,153 @@ app.set('view engine', 'handlebars');
 
 // Configure the home page to be the default.
 app.get('/', function (req, res) {
-	db.getItems('athletes', {}, function(err, athletes) {
-		if (err) sendError(res);
-		else res.render('home.handlebars', {
-			athletes : athletes
-		});
-	});
+  db.getItems('athletes', {}, function(err, athletes) {
+    if (err) sendError(res);
+    else res.render('home.handlebars', {
+      athletes : athletes
+    });
+  });
 });
 
 // Initiate OAuth registration of a new Strava athlete/user.
 app.get('/register', function(req, res) {
-	// Redirect the browser to the Strava OAuth grant page.
-	res.redirect(strava.getOAuthRequestAccessUrl());
+  // Redirect the browser to the Strava OAuth grant page.
+  res.redirect(strava.getOAuthRequestAccessUrl());
 });
 
 // Handle the OAuth callback from Strava, and exchange the temporary code for an access token.
 app.get('/registercode', function(req, res) {
-	var stravaCode = req.query.code;
+  var stravaCode = req.query.code;
 
-	if (stravaCode == null) {
-		var description = 'Query parameter "code" is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else {
-		registerAthlete(stravaCode, function(err) {
-			if (err) sendErrorMessage(res, "Unable to process Strava authorisation request");
-			else res.redirect('./');
-		});
-	}
+  if (stravaCode == null) {
+    var description = 'Query parameter "code" is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else {
+    registerAthlete(stravaCode, function(err) {
+      if (err) sendErrorMessage(res, 'Unable to process Strava authorisation request');
+      else res.redirect('./');
+    });
+  }
 });
 
 // Directly register a bearer token.  Primarily useful in a development setting.
 app.get('/registertoken', function(req, res) {
-	var stravaToken = req.query.token;
+  var stravaToken = req.query.token;
 
-	if (stravaToken == null) {
-		var description = 'Query parameter "token" is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else {
-		registerAthleteToken(stravaToken, function(err) {
-			if (err) sendErrorMessage(res, "Unable to register Strava token");
-			else res.redirect('./');
-		});
-	}
+  if (stravaToken == null) {
+    var description = 'Query parameter "token" is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else {
+    registerAthleteToken(stravaToken, function(err) {
+      if (err) sendErrorMessage(res, 'Unable to register Strava token');
+      else res.redirect('./');
+    });
+  }
 });
 
 // Display information available for a specific athlete.
 app.get('/athlete/:id', function(req, res) {
-	var athleteId = parseInt(req.params.id);
+  var athleteId = parseInt(req.params.id);
 
-	if (isNaN(athleteId)) {
-		var description = 'Athlete identifier is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else db.getItems('athletes', { id : athleteId }, function(err, athletes) {
-		if (err) sendError(res);
-		else db.getItems('activites', { athleteId : athleteId }, function(err, activities) {
-			if (err) sendError(res);
-			else res.render('athlete.handlebars', {
-				athlete: athletes[0],
-				activities: activities
-			});
-		});
-	});
+  if (isNaN(athleteId)) {
+    var description = 'Athlete identifier is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else db.getItems('athletes', { id : athleteId }, function(err, athletes) {
+    if (err) sendError(res);
+    else db.getItems('activites', { athleteId : athleteId }, function(err, activities) {
+      if (err) sendError(res);
+      else res.render('athlete.handlebars', {
+        athlete: athletes[0],
+        activities: activities
+      });
+    });
+  });
 });
 
 // Download athlete activities as a CSV.
 app.get('/athlete/:id/activitiescsv', function(req, res) {
-	var athleteId = parseInt(req.params.id);
+  var athleteId = parseInt(req.params.id);
 
-	if (isNaN(athleteId)) {
-		var description = 'Athlete identifier is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else db.getItems('activities', { "athlete.id" : athleteId }, function(err, activities) {
-		if (err) sendError(res);
-		else {
-			res.set({
-				'Content-Type': 'text/csv',
-				'Content-Disposition': 'attachment;filename=activities-' + athleteId + '.csv'
-			});
-			res.write('id,start_date_local,timezone,distance,moving_time,elapsed_time,total_elevation_gain,type,average_speed,max_speed,name\n');
-			for (var i = 0; i < activities.length; i++) {
-				var activity = activities[i];
-				res.write(
-					activity.id + ',' + activity.start_date_local + ',' + activity.timezone + ','
-					+ activity.distance + ',' + activity.moving_time + ',' + activity.elapsed_time + ','
-					+ activity.total_elevation_gain + ',' + activity.type + ',' + activity.average_speed + ','
-					+ activity.max_speed + ',' + util.csvString(activity.name) + '\n');
-			}
-			res.end();
-		}
-	});
+  if (isNaN(athleteId)) {
+    var description = 'Athlete identifier is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else db.getItems('activities', { 'athlete.id' : athleteId }, function(err, activities) {
+    if (err) sendError(res);
+    else {
+      res.set({
+        'Content-Type': 'text/csv',
+        'Content-Disposition': 'attachment;filename=activities-' + athleteId + '.csv'
+      });
+      res.write('id,start_date_local,timezone,distance,moving_time,elapsed_time,total_elevation_gain,type,average_speed,max_speed,name\n');
+      for (var i = 0; i < activities.length; i++) {
+        var activity = activities[i];
+        res.write(
+          activity.id + ',' + activity.start_date_local + ',' + activity.timezone + ','
+          + activity.distance + ',' + activity.moving_time + ',' + activity.elapsed_time + ','
+          + activity.total_elevation_gain + ',' + activity.type + ',' + activity.average_speed + ','
+          + activity.max_speed + ',' + util.csvString(activity.name) + '\n');
+      }
+      res.end();
+    }
+  });
 });
 
 // Refresh the athlete in the database.
 app.post('/athlete/:id/refresh', function(req, res) {
-	var athleteId = parseInt(req.params.id);
+  var athleteId = parseInt(req.params.id);
 
-	if (isNaN(athleteId)) {
-		var description = 'Athlete identifier is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else refreshAthlete(athleteId, function(err) {
-		if (err) sendError(res);
-		else res.redirect('../' + athleteId);
-	});
+  if (isNaN(athleteId)) {
+    var description = 'Athlete identifier is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else refreshAthlete(athleteId, function(err) {
+    if (err) sendError(res);
+    else res.redirect('../' + athleteId);
+  });
 });
 
 // Refresh all activities in the database for the athlete.
 app.post('/athlete/:id/refreshactivities', function(req, res) {
-	var athleteId = parseInt(req.params.id);
+  var athleteId = parseInt(req.params.id);
 
-	if (isNaN(athleteId)) {
-		var description = 'Athlete identifier is missing';
-		console.log(description);
-		sendErrorMessage(res, description);
-	} else refreshAthleteActivities(athleteId, function(err) {
-		if (err) sendError(res);
-		else res.redirect('../' + athleteId);
-	});
+  if (isNaN(athleteId)) {
+    var description = 'Athlete identifier is missing';
+    console.log(description);
+    sendErrorMessage(res, description);
+  } else refreshAthleteActivities(athleteId, function(err) {
+    if (err) sendError(res);
+    else res.redirect('../' + athleteId);
+  });
 });
 
 // Display the leaderboard.
 app.get('/leaderboard', function(req, res) {
-	db.getItems('athletes', {}, function(err, athletes) {
-		if (err) sendError(res);
-		else db.getItems('activities', {}, function(err, activities) {
-			if (err) sendError(res);
-			else {
-				var leaderboard = leaderboardEngine.build(athletes, activities);
-				res.render('leaderboard.handlebars', {
-					leaderboardjson : util.stringify(leaderboard),
-					leaderboard : leaderboard
-				});
-			}
-		});
-	});
+  db.getItems('athletes', {}, function(err, athletes) {
+    if (err) sendError(res);
+    else db.getItems('activities', {}, function(err, activities) {
+      if (err) sendError(res);
+      else {
+        var leaderboard = leaderboardEngine.build(athletes, activities);
+        res.render('leaderboard.handlebars', {
+          leaderboardjson : util.stringify(leaderboard),
+          leaderboard : leaderboard
+        });
+      }
+    });
+  });
 });
 
 // Create a HTTP listener.
 console.log('Creating HTTP listener');
 var server = app.listen(config.express.port, function() {
-	console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
 
 // Set up a regular refresh of athlete activities.
 setInterval(function() {
-	refreshAllAthleteActivities(function() {});
+  refreshAllAthleteActivities(function() {});
 }, 3600000);
