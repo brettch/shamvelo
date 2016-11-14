a28da910da25fb86366a6b64297801dcb81f744a
Brett Henderson <brett@bretth.com>
2016-11-14 13:47:17 +1000

    Fix remaining issues with home view rendering
    * Use correct path for template module
    * Parse raw S3 object into JSON
    * Use correct sortBy method in lodash
    * Call renderView in flatMap instead of map due to async processing
    * Pass render data in correct map structure

--- lambda/src/home.js
+++ lambda/src/home.js
@@ -1,22 +1,30 @@
 'use strict';
 
 module.exports = {
-  buildView
+  buildView,
+  getView
 };
 
 const _ = require('lodash');
 const config = require('./config');
 const s3 = require('./s3');
-const template = require('./src/template');
+const template = require('./template');
 
 function buildView() {
   return s3.loadObjects(`shamvelo-${config.environment}-athlete`)
+    .map(object => JSON.parse(object.toString()))
     .toArray()
-    .map(athletes => _.sort(athletes, athletes.name))
-    .map(renderView)
-    .flatMap(content => s3.upload(`shamvelo-${config.environment}-view`, 'home', content));
+    .map(athletes => _.sortBy(athletes, 'name'))
+    .flatMap(renderView)
+    .flatMap(content => s3.upload(`shamvelo-${config.environment}-view`, 'home', content))
+    .map(() => {});
 }
 
 function renderView(athletes) {
-  return template.render('home', athletes);
+  return template.render('home', { athletes });
+}
+
+function getView() {
+  return s3.getObject(`shamvelo-${config.environment}-view`, 'home')
+    .map(buffer => buffer.toString());
 }
