84026bbcd6e6a9494ac6856e0c5b45e757833ffd
Brett Henderson <brett@bretth.com>
2016-11-20 14:59:37 +1000

    Only upload activities to S3 if they've changed
    Preventing upload of changed activities will prevent unnecessary
    re-rendering of downstream views such as the leaderboard.

--- lambda/src/activity.js
+++ lambda/src/activity.js
@@ -9,8 +9,11 @@ module.exports = {
 const athlete = require('./athlete');
 const config = require('./config');
 const register = require('./register');
+const Rx = require('rx');
 const s3 = require('./s3');
 const strava = require('./strava');
+
+const rxo = Rx.Observable;
 
 function refreshAllActivities() {
   return athlete.getIds()
@@ -22,9 +25,29 @@ function refreshActivitiesForAthlete(athleteId) {
   return register.getTokenForAthlete(athleteId)
     .flatMap(strava.getActivities)
     .toArray()
-    .flatMap(activities =>
-      s3.upload(`shamvelo-${config.environment}-activity`, athleteId, JSON.stringify(activities))
+    .map(JSON.stringify)
+    // Load existing activities from S3 and check if they've changed
+    .flatMap(activitiesJson =>
+      s3
+        .getObject(`shamvelo-${config.environment}-activity`, athleteId)
+        .map(buffer => buffer.toString())
+        .map(s3ActivitiesJson => ({
+          activitiesJson: activitiesJson,
+          changed: activitiesJson != s3ActivitiesJson
+        }))
     )
+    // Only upload activities to S3 if they've changed
+    .flatMap(activitiesObj => {
+      if (activitiesObj.changed) {
+        return s3.upload(
+          `shamvelo-${config.environment}-activity`,
+          athleteId,
+          activitiesObj.activitiesJson
+        );
+      } else {
+        return rxo.return();
+      }
+    })
     .map(() => {});
 }
 
