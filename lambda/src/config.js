0b71923439c4cbe1bdf3c0557807565d9aa1a64a
Brett Henderson <brett.henderson@rea-group.com>
2017-05-16 22:10:55 +1000

    force loading of TZ from .env file
    As of May 2017, AWS Lambda is setting the TZ variable (to
    /etc/localtime).  The dotenv library won't overwrite existing
    environment variables which prevents the value in .env from taking
    effect.  This change explicitly clears any existing TZ variable prior to
    executing dotenv.config so that it sets TZ correctly.

--- lambda/src/config.js
+++ lambda/src/config.js
@@ -1,6 +1,11 @@
 'use strict';
 
-// Load strava environment variables.
+// Load environment variables.  We must clear TZ if it exists because dotenv
+// will refuse to overwrite existing variables and AWS Lambda has begun setting
+// it as of May 2017.
+if (process.env.TZ) {
+  delete process.env.TZ
+}
 require('dotenv').config();
 
 module.exports = {
