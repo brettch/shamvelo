178c89f4a64799d240dfa5f9910c19dc57cfa703
Brett Henderson <brett@bretth.com>
2024-01-12 13:23:16 +0800

    Add TypeScript support to eslint

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // Add this when we have proper types.
+    // 'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    '/.eslintrc.cjs',
+    "/dist",
+  ],
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
