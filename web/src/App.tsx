93c43aafc2efdbdcb615d87655048dbc9ec2253b
Brett Henderson <brett@bretth.com>
2024-03-29 16:56:36 +1100

    Add top level app/menu bar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { CssBaseline } from '@mui/material';
+import LeaderboardList from './LeaderboardList';
+import ResponsiveAppBar from './ResponsiveAppBar';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ResponsiveAppBar/>
+
+      <LeaderboardList/>
     </div>
   );
 }
